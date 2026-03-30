74f3c395f928bccc789d1f0cc42c1f9c7e27b60e
mizan <mizan389rahman@gmail.com>
2026-03-31 00:46:29 +0300

    2nd problem solved

--- elevator.py
+++ elevator.py
@@ -21,11 +21,34 @@ class Elevator:
             self.floor_down()
 
 
-#Main programme
-floor_elevator = Elevator(1, 10)
 
-floor_elevator.go_to_floor(5) #Moving up
-floor_elevator.go_to_floor(1) #Moving down
+#Main program
+# floor_elevator = Elevator(1, 10)
+#
+# floor_elevator.go_to_floor(5) #Moving up
+# floor_elevator.go_to_floor(1) #Moving down
 
 
 
+
+
+#2 Building class
+class Building:
+    def __init__(self, bottom, top, number_of_elevators):
+        self.elevators = []
+
+        for i in range(number_of_elevators):
+            elevator_object = Elevator(bottom, top)
+            self.elevators.append(elevator_object)
+
+    def run_elevator(self, number, target_floor):
+        elevator = self.elevators[number]
+        elevator.go_to_floor(target_floor)
+
+
+# Main program
+building = Building(1, 10, 3)
+
+building.run_elevator(0, 5)
+building.run_elevator(1, 7)
+building.run_elevator(0, 1)
